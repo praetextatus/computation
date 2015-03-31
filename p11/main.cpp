40274bf022ded6cafa86b5c6ac1e21f1fe416b2c
peter <pmostowsky@gmail.com>
2015-03-31 18:05:07 +0300

    Some shitty code.

--- p11/main.cpp
+++ p11/main.cpp
@@ -5,37 +5,43 @@ typedef Math::Matrix<double, 3, 3> Matrix3d;
 typedef Math::Matrix<double, 3, 1> Vector3d;
 
 int main() {
-	Matrix3d mat 	{2, -14, 8,
-			3, -22, 7,
-			0, 2, 5};
+	Math::Matrix<double, 4, 4> mat 	{2, -14, 8, 1,
+			3, -22, 7, 2,
+			0, 2, 5, -1,
+			3, -1, 3, 2};
 	
 	/* Invert matrix */
 	std::cout << "A\n" << mat;
-	Matrix3d inv;
+	Math::Matrix<double, 4, 4> inv;
 	invert(mat, inv);
 	std::cout << "A^(-1)\n" << inv;
 	
-    Vector3d vec{2, 0, 5};
-	Vector3d x;
+	Math::Matrix<double, 4, 1> vec{2, 0, 5, 0};
+	Math::Matrix<double, 4, 1> x;
 	std::cout << "\nSolving system Ax=b where b =\n" << vec;
-	Math::Matrix<double, 3, 4> ext = Math::concatenateH(mat, vec);
+	Math::Matrix<double, 4, 5> ext = Math::concatenateH(mat, vec);
 	gauss(ext, x);
 	std::cout << "x = \n" << x;
-	Vector3d R = vec + (-Math::dot(mat, x));
+	Math::Matrix<double, 4, 1> R = vec + (-Math::dot(mat, x));
 	std::cout << "R =\n" << R;
 
 	std::cout << "\n\nLU-Decomposition\n";
-	Math::Matrix<double, 3, 3> L, U;
+	Math::Matrix<double, 4, 4> L, U;
 	luDecomposition(mat, L, U);
 	std::cout << "L=\n" << L << "U\n" << U;
 
 	mat(0,0) = mat(0,0) * 1e-8;
 	std::cout << "\n\n\nSolving system Cx=b\n where C=\n" << mat;
 	ext = Math::concatenateH(mat, vec);
+	gauss(ext, x);	
+	std::cout << "x = \n" << x;
+	ext = Math::concatenateH(mat, vec);
+	R = vec + (-Math::dot(mat, x));
+	std::cout << "R1=\n" << R;
 	gauss(ext, x, true);
 	std::cout << "x = \n" << x;
 	R = vec + (-Math::dot(mat, x));
-	std::cout << "R =\n" << R;
+	std::cout << "R2 =\n" << R;
 
 }
 	
